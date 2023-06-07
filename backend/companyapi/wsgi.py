487a9f970d133b28bc49494b5626199c1eb1048e
Aashish <aaxishpunk31@gmail.com>
2023-06-07 23:07:34 +0545

    added gunicorn

--- backend/companyapi/wsgi.py
+++ backend/companyapi/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.companyapi.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "companyapi.settings")
 
 application = get_wsgi_application()
 
